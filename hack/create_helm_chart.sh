6b770ed6e4ae7df014212e3b757ac0c373eb1071
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-01-19 16:46:42 +0100

    feat: add helm chart for bootstrapper

---
+++ hack/create_helm_chart.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+CONTROLLER="${2}"
+
+check_dependency() {
+  if ! command -v helm &> /dev/null
+  then
+      echo "Helm could not be found. Please install Helm to proceed."
+      exit
+  fi
+    if ! command -v kustomize &> /dev/null
+    then
+        echo "kustomize could not be found. Please install kustomize to proceed."
+        exit
+    fi
+}
+
+create_helm_chart() {
+  helm create "${1}/${CONTROLLER}"
+  cd "${1}/${CONTROLLER}" || exit
+  rm -r templates
+  mkdir templates
+  mkdir crds
+  rm -r charts
+  rm values.yaml
+  cd ../helm_temp || exit
+
+  # Set chart app versiom to release version
+  RELEASE=$(ls -t ../../docs/release_notes | head -1 | sed "s/v//g" | sed "s/.md//g")
+
+  case "${1}" in
+    "helm" )
+      #mac
+      split -p "^---$" "install.yaml" "helm_";
+      sed -i "" "s/appVersion: .*/appVersion: \"${RELEASE}\"/g" "../${CONTROLLER}/Chart.yaml"
+
+      ;;
+    "output" )
+      #ubuntu
+      csplit "install.yaml" "/^---$/" {*} --prefix "helm_" -q;
+      sed -i "s/appVersion: .*/appVersion: \"${RELEASE}\"/g" "../${CONTROLLER}/Chart.yaml"
+      ;;
+    * )
+      exit
+      ;;
+  esac
+  rm install.yaml
+  #Move into crds & templates folders after renaming
+  HELM_FILES=($(ls ))
+  for input in "${HELM_FILES[@]}";  do
+      FILENAME=$(cat "${input}" | grep '^  name: ' | head -1 | sed 's/name: //g' | sed 's/\./_/g' | sed 's/ //g')
+      TYPE=$(cat "${input}" | grep '^kind: ' | head -1 | sed 's/kind: //g' | sed 's/\./_/g' | tr '[:upper:]' '[:lower:]' | sed 's/ //g')
+      if grep -q "kind: CustomResourceDefinition" "${input}" ; then
+          mv ${input} ../${CONTROLLER}/crds/${FILENAME}.yaml
+      else
+          mv ${input} ../${CONTROLLER}/templates/${TYPE}_${FILENAME}.yaml
+      fi
+  done
+  cd ..
+  rm -rf helm_temp
+}
+
+create_from_local_resource_manifests() {
+  echo "Creating from local manifests in the repository"
+  rm -rf helm
+  mkdir -p helm
+  mkdir -p "helm/helm_temp"
+  kustomize build ./config/default > ./helm/helm_temp/install.yaml
+
+  create_helm_chart "helm"
+}
+
+create_from_github_release() {
+  echo "Creating from the release"
+  mkdir -p "output/helm_temp"
+  cp ./output/install.yaml ./output/helm_temp/install.yaml
+  create_helm_chart "output"
+  tar -czf helm_chart.tar.gz ${CONTROLLER}
+}
+
+check_dependency
+case "${1}" in
+  "local" )
+      create_from_local_resource_manifests
+      ;;
+  "release" )
+      create_from_github_release
+      ;;
+  * )
+    echo -n "unknown"
+    exit
+    ;;
+esac
